3dce924f96b94735a18f4ab452ca4ef68045d8a6
yesicazemanate <ymzemanate@soy.sena.edu.co>
2024-09-09 12:16:34 -0500

    register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,7 +3,7 @@ import { FormRegister } from '../components/formRegister'
 export const Register = () => {
    
   return (
-    <div className='bg-auth-bg flex justify-center items-center h-full'>
+    <div className='bg-auth-bg flex justify-center items-center h-full bg-cover'>
     <FormRegister/>
     </div>
     
