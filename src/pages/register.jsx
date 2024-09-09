7bac4b3b8f8d0e7a831adb147cabc86867428408
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-09-09 12:17:13 -0500

    register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,7 +3,7 @@ import { FormRegister } from '../components/formRegister'
 export const Register = () => {
    
   return (
-    <div className='bg-auth-bg flex justify-center items-center h-full bg-cover'>
+    <div className='bg-auth-bg flex justify-center items-center h-screen bg-cover'>
     <FormRegister/>
     </div>
     
