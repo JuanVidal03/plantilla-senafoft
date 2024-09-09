01732929a119622670ccc16476d50ab6850d38e4
Juan Vidal <juanmvg2003@gmail.com>
2024-09-09 11:29:24 -0500

    configuracion de las rutas

---
+++ src/routes/AllRoutes.routes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+
+
+const AllRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/login" element={<Login/>} />
+        </Routes>
+    );
+}
+
+export default AllRoutes;
